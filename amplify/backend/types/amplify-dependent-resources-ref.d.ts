6581b5f6dd77621b6db3c9498559db1e4f851215
mac <mac@chads-mbp.lan>
2023-04-19 20:45:46 -0400

    chat

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "chatapp2": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "api": {
+    "chatapp2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "chatapp2": {
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string"
+    }
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import {Amplify} from 'aws-amplify'
+import awsconfig from '../src/aws-exports'
+Amplify.configure(awsconfig)
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
